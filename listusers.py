1bea1864c738a6e0dfe03bf164ed98ddf5d1d728
Phil Lembo <phil.lembo@gmail.com>
2020-12-11 11:29:45 -0500

    Fix formatting issues (flake8)

--- listusers.py
+++ listusers.py
@@ -7,7 +7,6 @@ The configuration file would look like this:
     "username": "your_username@your_tenant.com",
     "password": "This is a sample only. You better NOT persist your password.",
     "scope": ["User.Read"],
-        
     "endpoint": "https://graph.microsoft.com/v1.0/users"
 
 }
@@ -20,14 +19,13 @@ You can then run this sample with a JSON configuration file:
 
     python sample.py parameters.json
 """
-
-import sys  # For simplicity, we'll read config file from 1st CLI param sys.argv[1]
+# For simplicity, we'll read config file from 1st CLI param
+# sys.argv[1]#
+import sys
 import json
 import logging
-
 import requests
 import msal
-
 
 # Optional logging
 # logging.basicConfig(level=logging.DEBUG)
@@ -37,24 +35,29 @@ config = json.load(open(sys.argv[1]))
 # Create a preferably long-lived app instance which maintains a token cache.
 app = msal.PublicClientApplication(
     config["client_id"], authority=config["authority"],
-    # token_cache=...  # Default cache is in memory only.
-                       # You can learn how to use SerializableTokenCache from
-                       # https://msal-python.rtfd.io/en/latest/#msal.SerializableTokenCache
-    )
+)
 
+# token_cache=...
+# Default cache is in memory only.
+# You can learn how to use SerializableTokenCache from
+# https://msal-python.rtfd.io/en/latest/#msal.SerializableTokenCache
 # The pattern to acquire a token looks like this.
+
 result = None
 
 # Firstly, check the cache to see if this end user has signed in before
 accounts = app.get_accounts(username=config["username"])
 if accounts:
-    logging.info("Account(s) exists in cache, probably with token too. Let's try.")
+    logging.info("Account(s) exists in cache, probably with token too. \
+                  Let's try.")
     result = app.acquire_token_silent(config["scope"], account=accounts[0])
 
 if not result:
-    logging.info("No suitable token exists in cache. Let's get a new one from AAD.")
+    logging.info("No suitable token exists in cache. \
+                  Let's get a new one from AAD.")
     # See this page for constraints of Username Password Flow.
-    # https://github.com/AzureAD/microsoft-authentication-library-for-python/wiki/Username-Password-Authentication
+    # https://github.com/AzureAD/microsoft-authentication-library-for-python/ \
+    # wiki/Username-Password-Authentication
     result = app.acquire_token_by_username_password(
         config["username"], config["password"], scopes=config["scope"])
 
@@ -67,7 +70,9 @@ if "access_token" in result:
 else:
     print(result.get("error"))
     print(result.get("error_description"))
-    print(result.get("correlation_id"))  # You may need this when reporting a bug
-    if 65001 in result.get("error_codes", []):  # Not mean to be coded programatically, but...
+    print(result.get("correlation_id"))  # You may need this to report a bug
+    if 65001 in result.get("error_codes", []):
+        # Not meant to be coded programatically, but...
         # AAD requires user consent for U/P flow
-        print("Visit this to consent:", app.get_authorization_request_url(config["scope"]))
+        print("Visit this to consent:",
+              app.get_authorization_request_url(config["scope"]))
